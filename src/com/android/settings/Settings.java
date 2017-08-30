d89806c395fb5a21b10ec6bef7832eeaf198ea79
maxwen <max.weninger@gmail.com>
2017-08-31 00:30:09 +0200

    Integrate Atomic-OS Laboratory into Settings
    Change-Id: I083f3188852f61943c5b06ecf3dbeeef76d9bfd0
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -190,5 +190,7 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class UserAndAccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
+    // Laboratory
+    public static class AtomicSettingsActivity extends SettingsActivity {}
 
 }
