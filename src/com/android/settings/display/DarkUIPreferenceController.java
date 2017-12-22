f440a8cdd016b01303097f279013020fdeb45e8b
Kshitij Gupta <kshitijgm@gmail.com>
2017-12-22 08:44:27 +0000

    Implement PreferenceControllerMixin to DarkUI preference controller
    - Settings search crashes if the Controler doesn't implement PreferenceControllerMixin
    
    Change-Id: I2c3e616d9af987a0f8daed70fc9908e9ebb4977b
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- src/com/android/settings/display/DarkUIPreferenceController.java
+++ src/com/android/settings/display/DarkUIPreferenceController.java
@@ -23,13 +23,14 @@ import android.support.v7.preference.PreferenceScreen;
 import android.provider.Settings;
 
 import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settings.core.PreferenceControllerMixin;
 
 import libcore.util.Objects;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DarkUIPreferenceController extends AbstractPreferenceController implements
-        Preference.OnPreferenceChangeListener {
+        PreferenceControllerMixin, Preference.OnPreferenceChangeListener {
 
     private static final String SYSTEM_UI_THEME = "systemui_theme_style";
     private ListPreference mSystemUiThemeStyle;
